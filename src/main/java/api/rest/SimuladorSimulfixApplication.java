939e37b1098b1f8d8f6c06cb3f4906d3f6f97945
dev-gsilv <dev.gsilv@gmail.com>
2023-10-21 17:58:01 -0300

    Commit inicial do projeto

---
+++ src/main/java/api/rest/SimuladorSimulfixApplication.java
@@ -0,0 +1,13 @@
+package api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorSimulfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorSimulfixApplication.class, args);
+	}
+
+}
